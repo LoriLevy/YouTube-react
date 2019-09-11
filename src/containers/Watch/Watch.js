e37e309d68b1d2a121b39b2b1707e75a352f05a2
Lori Levy <lori@lorinet.com>
2019-09-10 20:45:37 -0700

    Added static meta data, video-info-box and comments

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './Watch.scss';
-import {VideoPreview} from '../../components/VideoPreview/VideoPreview';
 import {RelatedVideos} from '../../components/RelatedVideos/RelatedVideos';
-import { Divider } from 'semantic-ui-react';
+import {Video} from '../../components/Video/Video';
 
 export class Watch extends React.Component {
   render() {
     return (
-      <>
-        {/* Static for now */}
-        <VideoPreview horizontal={true}/>
-        <RelatedVideos/>
-        </>
+        <div className='watch-grid'>
+          <Video className='video' id='DqUQW3xyQ1c' />
+          <div className='metadata'>Metadata</div>
+          <div className='video-info-box'>video-info-box</div>
+          <div className='comments'>Comments</div>
+        
+        <RelatedVideos className='related-videos'/>
+        </div>
     );
   }
 }
