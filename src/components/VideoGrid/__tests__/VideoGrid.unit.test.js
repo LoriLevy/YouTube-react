7133a4d22b4632e5626491e0c2a58b3daeaf81bc
Lori Levy <lori@lorinet.com>
2019-10-03 12:55:05 -0700

    Fix tests in format-number.unit.test.js

--- src/components/VideoGrid/__tests__/VideoGrid.unit.test.js
+++ src/components/VideoGrid/__tests__/VideoGrid.unit.test.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
 import VideoGrid from "../VideoGrid";
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import {  Provider  } from 'react-redux';
+
+const middleware = [thunk];
+const mockStore = configureStore(middleware);
+const initialState = {
+  example: {}
+};
+const store = mockStore(initialState);
 
 describe("VideoGrid", () => {
   test("renders without props", () => {
-    const wrapper = shallow(<VideoGrid />);
+    const wrapper = shallow(<Provider store={store}><VideoGrid /></Provider>);
     expect(wrapper).toMatchSnapshot();
   });
 
--- src/utilities/__tests__/format-number.unit.test.js
+++ src/utilities/__tests__/format-number.unit.test.js
@@ -1,93 +1,49 @@
-import {getShortNumberString} from '../number-format';
+import {formatNumberWithCommas} from '../format-number';
 
-test('getShortNumberString(0)', () => {
-  expect(getShortNumberString(0)).toEqual('0');
+test('formatNumberWithCommas(0)', () => {
+  expect(formatNumberWithCommas(0)).toEqual('0');
 });
 
-test('getShortNumberString(9)', () => {
-  expect(getShortNumberString(9)).toEqual('9');
+test('formatNumberWithCommas(9)', () => {
+  expect(formatNumberWithCommas(9)).toEqual('9');
 });
 
-test('getShortNumberString(52)', () => {
-  expect(getShortNumberString(52)).toEqual('52');
+test('formatNumberWithCommas(52)', () => {
+  expect(formatNumberWithCommas(52)).toEqual('52');
 });
 
-test('getShortNumberString(456)', () => {
-  expect(getShortNumberString(456)).toEqual('456');
+test('formatNumberWithCommas(456)', () => {
+  expect(formatNumberWithCommas(456)).toEqual('456');
 });
 
-test('getShortNumberString(1001)', () => {
-  expect(getShortNumberString(1001)).toEqual('1.0K');
+test('formatNumberWithCommas(1001)', () => {
+  expect(formatNumberWithCommas(1001)).toEqual('1,001');
 });
 
-test('getShortNumberString(1099)', () => {
-  expect(getShortNumberString(1099)).toEqual('1.1K');
+test('formatNumberWithCommas(1099)', () => {
+  expect(formatNumberWithCommas(1099)).toEqual('1,099');
 });
 
-test('getShortNumberString(5298)', () => {
-  expect(getShortNumberString(5298)).toEqual('5.3K');
+test('formatNumberWithCommas(5298)', () => {
+  expect(formatNumberWithCommas(5298)).toEqual('5,298');
 });
 
-test('getShortNumberString(10053)', () => {
-  expect(getShortNumberString(10053)).toEqual('10.1K');
+test('formatNumberWithCommas(10053)', () => {
+  expect(formatNumberWithCommas(10053)).toEqual('10,053');
 });
 
-test('getShortNumberString(10100)', () => {
-  expect(getShortNumberString(10100)).toEqual('10.1K');
+test('formatNumberWithCommas(100000)', () => {
+  expect(formatNumberWithCommas(100000)).toEqual('100,000');
 });
 
-test('getShortNumberString(10999)', () => {
-  expect(getShortNumberString(10999)).toEqual('11.0K');
+test('formatNumberWithCommas(1000000)', () => {
+  expect(formatNumberWithCommas(1000000)).toEqual('1,000,000');
 });
 
-test('getShortNumberString(11732)', () => {
-  expect(getShortNumberString(11732)).toEqual('12K');
+test('formatNumberWithCommas(23000000)', () => {
+  expect(formatNumberWithCommas(23000000)).toEqual('23,000,000');
 });
 
-test('getShortNumberString(100000)', () => {
-  expect(getShortNumberString(100000)).toEqual('100K');
-});
-
-test('getShortNumberString(532000)', () => {
-  expect(getShortNumberString(532000)).toEqual('532K');
-});
-
-test('getShortNumberString(1000000)', () => {
-  expect(getShortNumberString(1000000)).toEqual('1M');
-});
-
-test('getShortNumberString(1230000)', () => {
-  expect(getShortNumberString(1230000)).toEqual('1.2M');
-});
-
-test('getShortNumberString(23000000)', () => {
-  expect(getShortNumberString(23000000)).toEqual('23M');
-});
-
-test('getShortNumberString(872000000)', () => {
-  expect(getShortNumberString(872000000)).toEqual('872M');
-});
-
-test('getShortNumberString(1000000000)', () => {
-  expect(getShortNumberString(1000000000)).toEqual('1B');
-});
-
-test('getShortNumberString(1500000000)', () => {
-  expect(getShortNumberString(1500000000)).toEqual('1.5B');
-});
-
-test('getShortNumberString(20000000000)', () => {
-  expect(getShortNumberString(20000000000)).toEqual('20B');
-});
-
-test('getShortNumberString(387000000000)', () => {
-  expect(getShortNumberString(387000000000)).toEqual('387B');
-});
-
-test('getShortNumberString(1000000000000)', () => {
-  expect(getShortNumberString(1000000000000)).toEqual('1T');
-});
-
-test('getShortNumberString(1800000000000)', () => {
-  expect(getShortNumberString(1800000000000)).toEqual('1.8T');
+test('formatNumberWithCommas(1000000000)', () => {
+  expect(formatNumberWithCommas(1000000000)).toEqual('1,000,000,000');
 });
